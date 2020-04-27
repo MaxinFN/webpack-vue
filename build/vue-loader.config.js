17a36a54147c12f65ae5556e11cc4984af239c12
wuguangjun <wuguangjun@nn.com>
2020-04-27 20:02:24 +0800

    dev

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -1,12 +1,10 @@
 module.exports = (isDev) => {
-    return {
-        preserveWhitepace: true,    //去掉多余空格
-        extractCSS: !isDev,            // 是否将.vue文件中的css分离出来
-        cssModules: {
-            localIndentName: isDev ? '[path]-[name]-[hash:base64:5]' : '[hash:base64:5]',
-            cameCase: true
-        }
-        // postcss
-        // hotReload   //根据环境变量来生成
-    }
+  return {
+    preserveWhitepace: true, // 去掉多余空格
+    extractCSS: !isDev, // 是否将.vue文件中的css分离出来
+    cssModules: {
+      localIndentName: isDev ? '[path]-[name]-[hash:base64:5]' : '[hash:base64:5]',
+      cameCase: true
+    }// postcss// hotReload   //根据环境变量来生成
+  }
 }
